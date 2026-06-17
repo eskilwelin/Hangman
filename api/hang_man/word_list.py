951468596a67c7748c1b5b94035165ca91fd7ef3
Eskil Welin <eskilwelin@gmail.com>
2026-06-17 11:23:14 +0200

    Clean code, read splitlines funktioner från lektionen

--- api/hang_man/word_list.py
+++ api/hang_man/word_list.py
@@ -1,19 +1,18 @@
-import random, os
+import os
+import random
 
 def random_word(difficulty):
-    script_dir = os.path.dirname(os.path.abspath(__file__))
-    hard_path = os.path.join(script_dir, "hangmanwords.txt")
-    easy_path = os.path.join(script_dir, "easywords.txt")
+    directory = os.path.dirname(os.path.abspath(__file__))
+    hard_path = os.path.join(directory, "hangmanwords.txt")
+    easy_path = os.path.join(directory, "easywords.txt")
+    
     match difficulty:
         case 1:
             f = open(hard_path)
         case _:
             f = open(easy_path) 
             
-    word_list = []
-    for word in f.readlines():
-        word_list.append(word.strip())
+    word_list = f.read().splitlines()
     random_word = random.randint(0, len(word_list) -1)
     f.close()
     return(word_list[random_word])
-
